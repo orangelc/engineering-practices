9caef60cec62987eade0afc81ffde898607b7df9
Orlando <isc.orangelc@gmail.com>
2018-09-24 14:35:48 -0500

    first commit

---
+++ EngineeringPractices.Domain/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineeringPractices.Domain
+{
+    public class Inventory
+    {
+        public bool Create(Product product)
+        {
+            return true;
+        }
+    }
+}
