ffaa0d5fd7dbf43d5c8cabad521c4624054b2316
Orlando <isc.orangelc@gmail.com>
2018-09-27 17:20:37 -0500

    Add moq framework

--- EngineeringPractices.Test/UnitTest1.cs
+++ EngineeringPractices.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EngineeringPractices.Domain;
+using Moq;
 
 namespace EngineeringPractices.Test
 {
@@ -8,11 +9,24 @@ namespace EngineeringPractices.Test
     public class UnitTest1
     {
 
+        Inventory inventory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            inventory = new Inventory();
+        }
+
+        [TestCleanup]
+        public void Dispose()
+        {
+
+        }
+
         [TestMethod]
         [ExpectedException(typeof(Exception))]
         public void Cuando_AgregoProductoNulo_Entonces_LanzarException()
         {
-            Inventory inventory = new Inventory();
             inventory.Create(null);
         }
 
@@ -20,7 +34,6 @@ namespace EngineeringPractices.Test
         [ExpectedException(typeof(Exception))]
         public void Cuando_AgregoProductoSinNombre_Entonces_LanzarException()
         {
-            Inventory inventory = new Inventory();
             inventory.Create(new Product { Id=1, Name=string.Empty, Price=10.0 });
         }
 
@@ -28,7 +41,6 @@ namespace EngineeringPractices.Test
         [ExpectedException(typeof(Exception))]
         public void Cuando_AgregoProductoConUnPrecioMayorACien_Entonces_LanzarException()
         {
-            Inventory inventory = new Inventory();
             inventory.Create(new Product { Id = 1, Name = "P001", Price = 101.0 });
         }
 
